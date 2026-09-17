6e406b44465962836bea868bfd39533b6b16a798
imad <j166a775433@gmail.com>
2026-09-17 18:45:53 +0100

    Fix bugs in test

--- app/test_hello.py
+++ app/test_hello.py
@@ -1,6 +1,6 @@
 import unittest
-from hello import hello
+from hello import say_hello
 
 class TestHello(unittest.TestCase):
     def test_say_hello(self):
-        self.assertEqual(hello.say_hello(), "Hello, world!")
+        self.assertEqual(say_hello(), "Hello, world!")
